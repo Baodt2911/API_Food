f04af9ca28c8d7dee5487901bcd4765cdde2e15c
Baodt2911 <dobao2911bs@gmail.com>
2023-07-01 00:08:33 +0700

    Done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 app.use(cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "*",
     methods: ['GET', 'POST'],
     credentials: true
 }))
