5e6d981db1f5318599e030b64813122d679e2f91
Baodt2911 <dobao2911bs@gmail.com>
2023-07-16 23:36:01 +0700

    Edit router data-user

--- routes/data-user.js
+++ routes/data-user.js
@@ -3,7 +3,7 @@ import dataUserController from '../controllers/dataUserController.js'
 import { checkUser } from '../middleware/auth.js'
 const router = express.Router()
 router.get('/userId/:id', checkUser, dataUserController.getDataUser)
-router.post('/add-cart/:userId', checkUser, dataUserController.addCart)
-router.delete('/remove-product/:userId', checkUser, dataUserController.removeProduct)
+router.post('/add-cart/:id', checkUser, dataUserController.addCart)
+router.delete('/remove-product/:id', checkUser, dataUserController.removeProduct)
 router.post('/add-history', checkUser, dataUserController.addOrderHistory)
 export default router
